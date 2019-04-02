1689a77b6f6306d6f5d957ce4aa66ee350a8e5f9
Mona Lisa <andrewstockus@gamil.com>
2019-04-02 12:55:35 -0400

    Push

---
+++ public/assets/css/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  h4: {
+    color: 'silver'
+  },
+  hover: {
+    backgroundColor: 'silver'
+  },
+  'hoverRed:hover': {
+    color: 'yellow'
+  },
+  'hover:hover': {
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  }
+});
